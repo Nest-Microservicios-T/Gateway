0b92f418d4b800980637496cba6e52ef67ce9c4b
KRocioCC <krociocalderon@gmail.com>
2026-03-02 16:17:56 -0400

    creando el NASTMODULE

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,22 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { NATS_SERVICE, envs } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
+// import { ClientsModule, Transport } from '@nestjs/microservices';
+// import { NATS_SERVICE, envs } from 'src/config';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
 
   imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.natsServers,
-        },
-      },
-    ]),
+    // ClientsModule.register([
+    //   {
+    //     name: NATS_SERVICE,
+    //     transport: Transport.NATS,
+    //     options: {
+    //       servers: envs.natsServers,
+    //     },
+    //   },
+    // ]),
+    NatsModule,
   ],
 })
 export class ProductsModule {}
